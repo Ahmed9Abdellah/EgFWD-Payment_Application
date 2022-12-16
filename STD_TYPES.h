b05a246090b57ccd6c000e012a98c99977d716b6
Ahmed9Abdellah <118623171+Ahmed9Abdellah@users.noreply.github.com>
2022-12-16 07:47:19 +0200

    Add files via upload

---
+++ STD_TYPES.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+typedef unsigned char         uint8_t;
+typedef signed char           int8_t;
+typedef unsigned short        uint16_t;
+typedef signed short          int16_t;
+typedef unsigned long         uint32_t;
+typedef signed long           int32_t;
+typedef unsigned long long    uint64_t;
+typedef signed long long      int64_t;
+typedef float                 float32_t;
+typedef double                float64_t;
+
+#endif 
---
+++ main.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
+#include<stdio.h>
+#include"Card/card.h"
+#include"Terminal/terminal.h"
+#include"Server/server.h"
+#include"Application/app.h"
+int main() {
+//{9000, BLOCKED, "100020003000400050"}
+//{8000, RUNNING,  "998877665544332211"}
+	//appStart();
+	
+	//ST_transaction_t* transData=0;
+//uint8_t errorstate= recieveTransactionDataTest(transData);
+//uint8_t ErrorState= listSavedTransactionsTest(transData);
+	//DisplayCardTest();
+	//DisplayTerminalTest();
+	//DisplayServerTest();
+}
